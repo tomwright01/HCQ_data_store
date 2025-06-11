e3de07c10626f2cbeba9bba5e24b0b452886eb5b
owen7690 <81187339+owen7690@users.noreply.github.com>
2025-06-11 14:09:33 -0400

    Update setup.sql

--- database/setup.sql
+++ database/setup.sql
@@ -5,3 +5,16 @@ Use HCQ;
 CREATE TABLE subjects(
     subject_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(50) NOT NULL);
+CREATE DATABASE IF NOT EXISTS HOSPITAL;
+
+USE HOSPITAL;
+
+CREATE DATABASE patients (
+  patient_id INT AUTO_INCREMENT PRIMARY KEY
+  name VARCHAR(100) NOT NULL, 
+  age INT 
+  gender ENUM ('Male', 'Female', 'Other') 
+  sickness_category TINYINT CHECK (sickness_category BETWEEN 1 AND 4),
+  admission_date DATABASE
+  notes TEXT
+);
