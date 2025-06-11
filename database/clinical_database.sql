8e1ce710f91fccd05064ad4c0141db0a15a6725f
Tom Wright <tom@maladmin.com>
2025-06-11 15:27:37 -0400

    Bugfixes

--- database/clinical_database.sql
+++ database/clinical_database.sql
@@ -5,17 +5,18 @@ Use HCQ;
 CREATE TABLE subjects(
     subject_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(50) NOT NULL);
+
 CREATE DATABASE IF NOT EXISTS HOSPITAL;
 
 USE HOSPITAL;
 
-CREATE DATABASE patients (
-  patient_id INT AUTO_INCREMENT PRIMARY KEY
+CREATE TABLE patients (
+  patient_id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL, 
-  age INT 
-  gender ENUM ('Male', 'Female', 'Other') 
+  age INT, 
+  gender ENUM ('Male', 'Female', 'Other'),
   sickness_category TINYINT CHECK (sickness_category BETWEEN 1 AND 4),
-  admission_date DATABASE
+  admission_date DATE,
   notes TEXT
 );
 
