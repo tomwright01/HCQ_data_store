3e2e333e1f3b307cf7b15f86bda16983d29c5bf5
Tom Wright <tom@maladmin.com>
2025-06-16 10:42:15 -0400

    Created database connection

--- web/data/var/www/html/index.php
+++ web/data/var/www/html/index.php
@@ -1,2 +1,18 @@
 <?php
-phpinfo();
+$servername = "mariadb";
+$username = "root";
+$password = "notgood";
+
+// Create database connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// phpinfo();
+?>
+
+
