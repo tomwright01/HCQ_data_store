ba17610c5ea6dd4f7710d09a2caf5f59c80ae73b
owen7690 <81187339+owen7690@users.noreply.github.com>
2025-06-16 15:17:56 -0400

    Update index.php

--- web/data/var/www/html/index.php
+++ web/data/var/www/html/index.php
@@ -1,4 +1,3 @@
-
 <?php
 $servername = "mariadb";
 $username = "root";
@@ -12,131 +11,58 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
+// Query to get total number of patients
+$sql = "SELECT COUNT(*) AS total_patients FROM Patients";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$total_patients = $row['total_patients'];
+
+// Query to get average age of patients
+$sql_age = "SELECT AVG(YEAR(CURRENT_DATE) - year_of_birth) AS average_age FROM Patients";
+$result_age = $conn->query($sql_age);
+$row_age = $result_age->fetch_assoc();
+$average_age = $row_age['average_age'];
+
+// Query to get the count of males and females
+$sql_gender = "SELECT gender, COUNT(*) AS count FROM Patients GROUP BY gender";
+$result_gender = $conn->query($sql_gender);
+$gender_data = [];
+while ($row_gender = $result_gender->fetch_assoc()) {
+    $gender_data[$row_gender['gender']] = $row_gender['count'];
+}
+
 ?>
 
-<h2>Enter Patient and Visit Information</h2>
-<form action="submit.php" method="post">
-    <!-- Patient Information -->
-    <h3>Patient Information</h3>
-    <label for="location">Location:</label>
-    <input type="text" name="location" required><br><br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patient Data Overview</title>
+</head>
+<body>
+    <h1>Welcome to the Patient Data Portal</h1>
+    
+    <h2>Patient Summary</h2>
+    <p>Total number of patients: <?php echo $total_patients; ?></p>
+    <p>Average age of patients: <?php echo round($average_age, 2); ?> years</p>
+    <p>Number of males: <?php echo isset($gender_data['m']) ? $gender_data['m'] : 0; ?></p>
+    <p>Number of females: <?php echo isset($gender_data['f']) ? $gender_data['f'] : 0; ?></p>
+    
+    <h2>Add New Patient and Visit</h2>
+    <p>Click below to add a new patient and visit:</p>
+    <a href="form.php">Go to the form</a>
 
-    <label for="disease_id">Disease ID (1 = Lupus, 2 = RA, etc.):</label>
-    <input type="number" name="disease_id" required><br><br>
-
-    <label for="year_of_birth">Year of Birth:</label>
-    <input type="number" name="year_of_birth" required><br><br>
-
-    <label for="gender">Gender:</label>
-    <input type="radio" name="gender" value="m"> Male
-    <input type="radio" name="gender" value="f"> Female<br><br>
-
-    <label for="referring_doctor">Referring Doctor:</label>
-    <input type="text" name="referring_doctor" required><br><br>
-
-    <label for="rx_OD">Prescription OD:</label>
-    <input type="text" name="rx_OD" required><br><br>
-
-    <label for="rx_OS">Prescription OS:</label>
-    <input type="text" name="rx_OS" required><br><br>
-
-    <label for="procedures_done">Procedures Done:</label>
-    <textarea name="procedures_done"></textarea><br><br>
-
-    <label for="dosage">Dosage:</label>
-    <input type="text" name="dosage" required><br><br>
-
-    <label for="duration">Duration:</label>
-    <input type="number" name="duration" required><br><br>
-
-    <label for="cumulative_dosage">Cumulative Dosage:</label>
-    <input type="text" name="cumulative_dosage"><br><br>
-
-    <label for="date_of_discontinuation">Date of Discontinuation:</label>
-    <input type="date" name="date_of_discontinuation"><br><br>
-
-    <label for="extra_notes">Extra Notes:</label>
-    <textarea name="extra_notes"></textarea><br><br>
-
-    <!-- Visit Information -->
-    <h3>Visit Information</h3>
-    <label for="visit_date">Visit Date:</label>
-    <input type="date" name="visit_date" required><br><br>
-
-    <label for="visit_notes">Visit Notes:</label>
-    <textarea name="visit_notes"></textarea><br><br>
-
-    <!-- FAF Data -->
-    <h4>FAF Data</h4>
-    <label for="faf_test_id_OD">FAF Test ID (OD):</label>
-    <input type="number" name="faf_test_id_OD"><br><br>
-
-    <label for="faf_image_number_OD">FAF Image Number (OD):</label>
-    <input type="number" name="faf_image_number_OD"><br><br>
-
-    <label for="faf_test_id_OS">FAF Test ID (OS):</label>
-    <input type="number" name="faf_test_id_OS"><br><br>
-
-    <label for="faf_image_number_OS">FAF Image Number (OS):</label>
-    <input type="number" name="faf_image_number_OS"><br><br>
-
-    <!-- OCT Data -->
-    <h4>OCT Data</h4>
-    <label for="oct_test_id_OD">OCT Test ID (OD):</label>
-    <input type="number" name="oct_test_id_OD"><br><br>
-
-    <label for="oct_image_number_OD">OCT Image Number (OD):</label>
-    <input type="number" name="oct_image_number_OD"><br><br>
-
-    <label for="oct_test_id_OS">OCT Test ID (OS):</label>
-    <input type="number" name="oct_test_id_OS"><br><br>
-
-    <label for="oct_image_number_OS">OCT Image Number (OS):</label>
-    <input type="number" name="oct_image_number_OS"><br><br>
-
-    <!-- VF Data -->
-    <h4>VF Data</h4>
-    <label for="vf_test_id_OD">VF Test ID (OD):</label>
-    <input type="number" name="vf_test_id_OD"><br><br>
-
-    <label for="vf_image_number_OD">VF Image Number (OD):</label>
-    <input type="number" name="vf_image_number_OD"><br><br>
-
-    <label for="vf_test_id_OS">VF Test ID (OS):</label>
-    <input type="number" name="vf_test_id_OS"><br><br>
-
-    <label for="vf_image_number_OS">VF Image Number (OS):</label>
-    <input type="number" name="vf_image_number_OS"><br><br>
-
-    <!-- MFERG Data -->
-    <h4>MFERG Data</h4>
-    <label for="mferg_test_id_OD">MFERG Test ID (OD):</label>
-    <input type="number" name="mferg_test_id_OD"><br><br>
-
-    <label for="mferg_image_number_OD">MFERG Image Number (OD):</label>
-    <input type="number" name="mferg_image_number_OD"><br><br>
-
-    <label for="mferg_test_id_OS">MFERG Test ID (OS):</label>
-    <input type="number" name="mferg_test_id_OS"><br><br>
-
-    <label for="mferg_image_number_OS">MFERG Image Number (OS):</label>
-    <input type="number" name="mferg_image_number_OS"><br><br>
-
-    <!-- MERCI Ratings -->
-    <h4>MERCI Ratings</h4>
-    <label for="merci_rating_left_eye">MERCI Rating (Left Eye):</label>
-    <input type="number" name="merci_rating_left_eye"><br><br>
-
-    <label for="merci_rating_right_eye">MERCI Rating (Right Eye):</label>
-    <input type="number" name="merci_rating_right_eye"><br><br>
-
-    <input type="submit" value="Submit">
-</form>
+</body>
+</html>
 
 <?php
 // Close connection
 $conn->close();
 ?>
+
+
 
 
 
