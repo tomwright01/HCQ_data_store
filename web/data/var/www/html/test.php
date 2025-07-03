71da8354e67c7fbbdf7bcd771384e3b72a7d5d41
owenc7690 <owenchoy88@gmail.com>
2025-07-03 12:07:17 -0400

    commit

---
+++ web/data/var/www/html/test.php
@@ -0,0 +1,12 @@
+<HTML>
+    <head>
+    </head>
+    <body>
+        <h1>Test Page</h1>
+        <p>This is a test page to verify that the web server is running correctly.</p>
+        <p>Current time: <?php echo date('Y-m-d H:i:s'); ?></p>
+        <p>Data directory: <?php echo realpath('/var/www/data'); ?></p>
+
+        <img src="data/FAF/12_OD_20250505.png" alt="Test Image" style="max-width: 100%; height: auto;">
+    </body>
+</HTML> 
