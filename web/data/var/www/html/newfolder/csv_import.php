ef952ea728a8fcfbd8b0e28f775404fb0b474a84
owen7690 <81187339+owen7690@users.noreply.github.com>
2025-07-10 16:54:07 -0400

    Update csv_import.php

--- web/data/var/www/html/newfolder/csv_import.php
+++ web/data/var/www/html/newfolder/csv_import.php
@@ -234,7 +234,7 @@ function insertTest($conn, $testData) {
         $testData['oct_score'],
         $testData['vf_score']
     );
-    die(serialize($testData));
+    die(json_encode($testData));
     if (!$stmt->execute()) {
         throw new Exception("Test insert failed: " . $stmt->error);
     }
