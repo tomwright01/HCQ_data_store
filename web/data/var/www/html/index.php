4173d020ff69b84bbd11ff2cb97909d2dcd12a2e
owen7690 <81187339+owen7690@users.noreply.github.com>
2025-08-07 12:46:33 -0400

    Update index.php

--- web/data/var/www/html/index.php
+++ web/data/var/www/html/index.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require_once __DIR__ . '/config.php';
+require_once 'config.php';
 
 // Fetch all test data with related patient info
 $sql = "
