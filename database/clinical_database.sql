0f85e8699fbcaaa2ce42fcccbd9774d8661bdb98
owen7690 <81187339+owen7690@users.noreply.github.com>
2025-08-10 23:11:02 -0400

    Update clinical_database.sql

--- database/clinical_database.sql
+++ database/clinical_database.sql
@@ -38,7 +38,6 @@ CREATE TABLE audit_log (
 -- TESTS TABLE
 -- ==========================
 CREATE TABLE tests (
-    test_id VARCHAR(25) PRIMARY KEY,
     patient_id VARCHAR(25) NOT NULL,
     location ENUM('KH', 'CHUSJ', 'IWK', 'IVEY') DEFAULT 'KH',
     date_of_test DATE NOT NULL,
@@ -53,7 +52,7 @@ CREATE TABLE tests (
 -- TEST_EYES TABLE
 -- ==========================
 CREATE TABLE test_eyes (
-    test_id VARCHAR(25),
+    test_id VARCHAR(25) PRIMARY KEY,
     eye ENUM('OD', 'OS') NOT NULL,
     age TINYINT UNSIGNED NULL,
     report_diagnosis ENUM('normal','abnormal', 'exclude','no input') NOT NULL DEFAULT 'no input',
@@ -64,7 +63,7 @@ CREATE TABLE test_eyes (
     faf_grade TINYINT UNSIGNED NULL,
     oct_score DECIMAL(10,2) NULL,
     vf_score DECIMAL(10,2) NULL,
-    actual_diagnosis ENUM('RA','SLE','Sjogren','other') NOT NULL DEFAULT 'other',
+    actual_diagnosis VARCHAR(255) NULL,
     medication_name VARCHAR(100) NULL,
     dosage DECIMAL(10,2) NULL,
     dosage_unit VARCHAR(10) DEFAULT 'mg',
