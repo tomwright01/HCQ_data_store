63a4582e693d8298b70448680b92e6c5f82bc3f9
owen7690 <81187339+owen7690@users.noreply.github.com>
2025-08-26 10:26:19 -0400

    Update import_images.php

--- web/data/var/www/html/import_images.php
+++ web/data/var/www/html/import_images.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/includes/config.php';
-require_once __DIR__ . '/includes/functions.php';
+require_once 'config.php';
+require_once 'functions.php';
 
 set_time_limit(0);
 ini_set('memory_limit', '1024M');
